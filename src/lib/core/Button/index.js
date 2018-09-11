f5c7bcbe608fa6709df3a71714b8d68080331a79
Enric <enriccc15@gmail.com>
2018-09-11 15:29:58 +0200

    Mas estilos y modificaciones

--- src/lib/core/Button/index.js
+++ src/lib/core/Button/index.js
@@ -1,41 +1,77 @@
 // @flow
 import * as React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import colors from '../../helpers/colors';
 import { mainFont } from '../../helpers/fonts';
 
 const StyledButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   position: relative;
   border-radius: 50px;
   padding: 10px 20px;
   border: 0;
-  background-image: linear-gradient(
-    to right,
-    ${colors.buttonBackground.purpleGradient},
-    ${colors.buttonBackground.pinkGradient}
-  );
-  color: #fff;
+  background: ${props => (props.options.outLined ? 'transparent' : null)};
+  background-image: ${props =>
+    !props.options.outLined && !props.options.bgColor
+      ? `linear-gradient(
+      to right,
+      ${colors.buttonBackground.purpleGradient},
+      ${colors.buttonBackground.pinkGradient}
+    ); `
+      : null};
+  border: ${props => (props.options.outLined ? '2px solid' : null)};
+  background-color: ${props =>
+    props.options.bgColor && !props.options.outLined
+      ? props.options.bgColor
+      : null};
+  color: ${props =>
+    props.options.textColor ? props.options.textColor : '#fff'};
   cursor: pointer;
   font-family: ${mainFont};
   font-size: 1rem;
   font-weight: 600;
-  width: 300px;
+  width: ${props =>
+    props.options.icon && !props.options.label ? '0px' : '300px'};
 
   &:focus {
     outline: 0;
   }
 `;
 
+const ExtendedIcon = styled(FontAwesomeIcon)`
+  position: ${props =>
+    props.options.icon && props.options.label ? 'absolute' : 'initial'};
+  left: ${props => (props.options.icon && props.options.label ? '15px' : null)};
+`;
+
 type Props = {
-  label: string,
+  label?: React.Node,
   onClick?: Function,
+  icon?: React.Node,
+  bgColor?: string,
+  outLined?: boolean,
+  textColor?: string,
 };
 
 const WelcomeText = (props: Props) => {
-  const { label, onClick } = props;
+  const { label, onClick, icon, bgColor, outLined, textColor, ...rest } = props;
 
-  return <StyledButton onClick={onClick}>{label}</StyledButton>;
+  return (
+    <StyledButton
+      onClick={onClick}
+      options={{ icon, label, bgColor, outLined, textColor }}
+      {...rest}
+    >
+      {icon ? (
+        <ExtendedIcon icon={icon} options={{ icon, label }} size="lg" />
+      ) : null}
+      {label}
+    </StyledButton>
+  );
 };
 
 export default WelcomeText;
--- src/lib/core/Button/index.stories.js
+++ src/lib/core/Button/index.stories.js
@@ -1,9 +1,49 @@
 // @flow
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { faPlus, faWarehouse } from '@fortawesome/pro-light-svg-icons';
 
 import Button from './index';
 
-storiesOf('Button', module).add('Default', () => (
-  <Button label="Test text" onClick={() => console.log('hola')} />
+const actions = {
+  onClick: action('onClick'),
+};
+
+storiesOf('Button', module).add('With text', () => (
+  <Button label="Test text" onClick={() => console.log('hola')} {...actions} />
+));
+
+storiesOf('Button', module).add('With icon', () => (
+  <Button icon={faPlus} onClick={() => console.log('hola')} {...actions} />
+));
+
+storiesOf('Button', module).add('With icon and text', () => (
+  <Button
+    icon={faPlus}
+    label="Test text"
+    onClick={() => console.log('hola')}
+    {...actions}
+  />
+));
+
+storiesOf('Button', module).add('With solid background color', () => (
+  <Button
+    icon={faWarehouse}
+    label="Test text"
+    bgColor="#4CAF50"
+    onClick={() => console.log('hola')}
+    {...actions}
+  />
+));
+
+storiesOf('Button', module).add('Outlined', () => (
+  <Button
+    outLined
+    icon={faWarehouse}
+    label="Test text"
+    textColor="tomato"
+    onClick={() => console.log('hola')}
+    {...actions}
+  />
 ));
