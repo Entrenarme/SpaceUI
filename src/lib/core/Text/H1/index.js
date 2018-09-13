dc22a2820a0e1b2e27c935a1952127177e4fa645
Enric <enriccc15@gmail.com>
2018-09-13 10:42:44 +0200

    Mejora de estilos y añadido slick

--- src/lib/core/Text/H1/index.js
+++ src/lib/core/Text/H1/index.js
@@ -21,18 +21,26 @@ const ExtendedH1 = styled.h1`
   font-size: ${props => setFontSize(props.options.size)};
   font-weight: 700;
   color: ${props => props.options.color};
+  text-align: ${props =>
+    props.options.textAlign ? props.options.textAlign : 'left'};
 `;
 
 type Props = {
   children: React.Node,
   color?: string,
   size?: 'l' | 'm' | 's',
+  style?: Object,
+  textAlign?: string,
 };
 
 const H1 = (props: Props) => {
-  const { children, color, size } = props;
+  const { children, color, size, style, textAlign } = props;
 
-  return <ExtendedH1 options={{ color, size }}>{children}</ExtendedH1>;
+  return (
+    <ExtendedH1 options={{ color, size, textAlign }} style={style}>
+      {children}
+    </ExtendedH1>
+  );
 };
 
 export default H1;
