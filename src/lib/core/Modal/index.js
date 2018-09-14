7323c41032846c3cd46cdc4a3edcc9d1d34fe7c6
Enric <enriccc15@gmail.com>
2018-09-14 13:52:10 +0200

    Mejora del modal

--- src/lib/core/Modal/index.js
+++ src/lib/core/Modal/index.js
@@ -1,6 +1,9 @@
 // @flow
 import * as React from 'react';
 import Modal from 'react-modal';
+import styled from 'styled-components';
+
+import './styles/custom.css';
 
 const customStyles = {
   content: {
@@ -15,6 +18,8 @@ const customStyles = {
 
 type Props = {
   children: React.Node,
+  open: boolean,
+  onClose: Function,
 };
 
 type State = {
@@ -22,28 +27,15 @@ type State = {
 };
 
 class Dialog extends React.Component<Props, State> {
-  state = {
-    open: false,
-  };
-
-  openModal = () => {
-    this.setState({ open: true });
-  };
-
-  closeModal = () => {
-    this.setState({ open: false });
-  };
-
   render() {
-    const { open } = this.state;
-    const { children } = this.props;
+    const { children, open, onClose } = this.props;
 
     return (
       <Modal
         isOpen={open}
-        onRequestClose={this.closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
+        onRequestClose={() => onClose()}
+        className="Modal"
+        overlayClassName="Overlay"
       >
         {children}
       </Modal>
--- src/lib/core/Modal/index.stories.js
+++ src/lib/core/Modal/index.stories.js
@@ -3,10 +3,34 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
+import H2 from '../Text/H2/index';
+
 import Dialog from './index';
 
 const actions = {
   onClick: action('onClick'),
 };
 
-storiesOf('Dialog', module).add('Normal', () => <Dialog open>Header 1</Dialog>);
+class ModalTestEnviroment extends React.Component<Props, State> {
+  state = {
+    openModal: false,
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.setState({ openModal: true })}>
+          Open Modal
+        </button>
+        <Dialog
+          open={this.state.openModal}
+          onClose={() => this.setState({ openModal: false })}
+        >
+          <H2>Hola</H2>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+storiesOf('Dialog', module).add('Normal', () => <ModalTestEnviroment />);
