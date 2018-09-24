e981e22f10204486b99e63659eb71d6cfbc0c275
Enric <enriccc15@gmail.com>
2018-09-24 12:03:13 +0200

    Corrección de estilos

--- src/lib/core/Button/index.js
+++ src/lib/core/Button/index.js
@@ -11,7 +11,7 @@ const StyledButton = styled.button`
   justify-content: center;
   position: relative;
   border-radius: 50px;
-  padding: 15px 20px;
+  padding: ${props => (props.options.outLined ? '13px 20px' : '15px 20px')};
   border: 0;
   background: ${props => (props.options.outLined ? 'transparent' : null)};
   background-image: ${props =>
@@ -45,42 +45,7 @@ const StyledButton = styled.button`
 const ExtendedIconContainer = styled.div`
   position: ${props =>
     props.options.icon && props.options.label ? 'absolute' : 'initial'};
-  left: ${props => (props.options.icon && props.options.label ? '15px' : null)};
-`;
-
-const GlobalInputContainer = styled.div`
-  display: flex;
-  margin-top: 5px;
-`;
-
-const IconContainer = styled.div`
-  width: 50px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  border: 1px solid ${colors.gray.light};
-  border-radius: 50px 0px 0px 50px;
-  background-color: ${colors.gray.light};
-`;
-
-const ExtendedButton = styled.button`
-  display: flex;
-  padding-left: 15px !important;
-  cursor: pointer;
-  border: 1px solid ${colors.gray.light};
-  width: 260px;
-  padding: 15px;
-  border-radius: 0px 50px 50px 0px;
-  font-size: 14px;
-  font-family: ${mainFont};
-  color: ${colors.gray.dark};
-  background-color: #fff;
-  margin: 0;
-
-  &:focus {
-    outline: 0;
-  }
+  left: ${props => (props.options.icon && props.options.label ? '20px' : null)};
 `;
 
 type Props = {
@@ -108,20 +73,8 @@ const Button = (props: Props) => {
     type,
     disabled,
     containerIcon,
-    fixedIcon,
     ...rest
   } = props;
-
-  if (fixedIcon) {
-    return (
-      <GlobalInputContainer>
-        {icon ? <IconContainer onClick={onClick}>{icon}</IconContainer> : null}
-        <ExtendedButton onClick={onClick} {...rest}>
-          {children || label}
-        </ExtendedButton>
-      </GlobalInputContainer>
-    );
-  }
 
   return (
     <StyledButton
--- src/lib/core/Button/index.stories.js
+++ src/lib/core/Button/index.stories.js
@@ -12,92 +12,123 @@ const actions = {
   onClick: action('onClick'),
 };
 
-storiesOf('Button/Normal', module).add('With text', () => (
-  <Button label="Test text" onClick={() => console.log('hola')} {...actions} />
-));
+const ElementDecoration = storyFn => (
+  <div style={{ backgroundColor: '#F1F1F1', height: '100vh', padding: '20px' }}>
+    {storyFn()}
+  </div>
+);
 
-storiesOf('Button/Normal', module).add('With icon', () => (
-  <Button
-    icon={<FontAwesomeIcon icon={faWhatsapp} size="lg" />}
-    onClick={() => console.log('hola')}
-    {...actions}
-  />
-));
+storiesOf('Button/Normal', module)
+  .addDecorator(ElementDecoration)
+  .add('With text', () => (
+    <Button
+      label="Test text"
+      onClick={() => console.log('hola')}
+      {...actions}
+    />
+  ));
 
-storiesOf('Button/Normal', module).add('With icon and text', () => (
-  <Button
-    icon={<FontAwesomeIcon icon={faWhatsapp} size="lg" />}
-    label="Test text"
-    onClick={() => console.log('hola')}
-    {...actions}
-  />
-));
+storiesOf('Button/Normal', module)
+  .addDecorator(ElementDecoration)
+  .add('With icon', () => (
+    <Button
+      icon={<FontAwesomeIcon icon={faWhatsapp} size="lg" />}
+      onClick={() => console.log('hola')}
+      {...actions}
+    />
+  ));
 
-storiesOf('Button/Solid', module).add('With text', () => (
-  <Button
-    label="Test text"
-    bgColor="#4CAF50"
-    onClick={() => console.log('hola')}
-    {...actions}
-  />
-));
+storiesOf('Button/Normal', module)
+  .addDecorator(ElementDecoration)
+  .add('With icon and text', () => (
+    <Button
+      icon={<FontAwesomeIcon icon={faWhatsapp} size="lg" />}
+      label="Test text"
+      onClick={() => console.log('hola')}
+      {...actions}
+    />
+  ));
 
-storiesOf('Button/Solid', module).add('With icon', () => (
-  <Button
-    icon={<FontAwesomeIcon icon={faWhatsapp} size="lg" />}
-    bgColor="#4CAF50"
-    onClick={() => console.log('hola')}
-    {...actions}
-  />
-));
+storiesOf('Button/Solid', module)
+  .addDecorator(ElementDecoration)
+  .add('With text', () => (
+    <Button
+      label="Test text"
+      bgColor="#4CAF50"
+      onClick={() => console.log('hola')}
+      {...actions}
+    />
+  ));
 
-storiesOf('Button/Solid', module).add('With icon and text', () => (
-  <Button
-    icon={<FontAwesomeIcon icon={faWhatsapp} size="lg" />}
-    label="Test text"
-    bgColor="#4CAF50"
-    onClick={() => console.log('hola')}
-    {...actions}
-  />
-));
+storiesOf('Button/Solid', module)
+  .addDecorator(ElementDecoration)
+  .add('With icon', () => (
+    <Button
+      icon={<FontAwesomeIcon icon={faWhatsapp} size="lg" />}
+      bgColor="#4CAF50"
+      onClick={() => console.log('hola')}
+      {...actions}
+    />
+  ));
 
-storiesOf('Button/Outlined', module).add('With text', () => (
-  <Button
-    outLined
-    label="Test text"
-    textColor="tomato"
-    onClick={() => console.log('hola')}
-    {...actions}
-  />
-));
+storiesOf('Button/Solid', module)
+  .addDecorator(ElementDecoration)
+  .add('With icon and text', () => (
+    <Button
+      icon={<FontAwesomeIcon icon={faWhatsapp} size="lg" />}
+      label="Test text"
+      bgColor="#4CAF50"
+      onClick={() => console.log('hola')}
+      {...actions}
+    />
+  ));
 
-storiesOf('Button/Outlined', module).add('With icon', () => (
-  <Button
-    outLined
-    icon={<FontAwesomeIcon icon={faWhatsapp} size="lg" />}
-    textColor="tomato"
-    onClick={() => console.log('hola')}
-    {...actions}
-  />
-));
+storiesOf('Button/Solid', module)
+  .addDecorator(ElementDecoration)
+  .add('Payment example', () => (
+    <Button
+      icon={<FontAwesomeIcon icon={faPaypal} size="lg" />}
+      textColor="#666666"
+      label="PAYMENT EXAMPLE"
+      bgColor="#fff"
+      onClick={() => console.log('hola')}
+      {...actions}
+    />
+  ));
 
-storiesOf('Button/Outlined', module).add('With icon and text', () => (
-  <Button
-    outLined
-    icon={<FontAwesomeIcon icon={faWhatsapp} size="lg" />}
-    label="Test text"
-    textColor="tomato"
-    onClick={() => console.log('hola')}
-    {...actions}
-  />
-));
+storiesOf('Button/Outlined', module)
+  .addDecorator(ElementDecoration)
+  .add('With text', () => (
+    <Button
+      outLined
+      label="Test text"
+      textColor="tomato"
+      onClick={() => console.log('hola')}
+      {...actions}
+    />
+  ));
 
-storiesOf('Button/FixedIcon', module).add('Normal', () => (
-  <Button
-    fixedIcon
-    icon={<FontAwesomeIcon icon={faPaypal} size="lg" />}
-    label="Send email"
-    onClick={() => console.log('hola')}
-    {...actions}
-  />
-));
+storiesOf('Button/Outlined', module)
+  .addDecorator(ElementDecoration)
+  .add('With icon', () => (
+    <Button
+      outLined
+      icon={<FontAwesomeIcon icon={faWhatsapp} size="lg" />}
+      textColor="tomato"
+      onClick={() => console.log('hola')}
+      {...actions}
+    />
+  ));
+
+storiesOf('Button/Outlined', module)
+  .addDecorator(ElementDecoration)
+  .add('With icon and text', () => (
+    <Button
+      outLined
+      icon={<FontAwesomeIcon icon={faWhatsapp} size="lg" />}
+      label="Test text"
+      textColor="tomato"
+      onClick={() => console.log('hola')}
+      {...actions}
+    />
+  ));
